9631787d41cfcece2f05d443be0b69939ac4ac62
phucbndx <phucnguyendx9012@gmail.com>
2023-11-24 00:08:22 +0700

    get data to mongodb

--- index.js
+++ index.js
@@ -3,17 +3,39 @@ import authRouter from './routers/auth.js'
 import studentRouter from './routers/student.js'
 import teacherRouter from './routers/teacher.js'
 import todoRouter from './routers/todoRouter.js'
-
-const app = express()
-app.use(express.json())
-app.use('/api/v1/auth', authRouter)
-app.use("/students", studentRouter)
-app.use("/teachers", teacherRouter)
-app.use("/api/v1/todos", todoRouter)
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+import { MongoClient } from 'mongodb'
 
 
+async function main() {
+  const app = express()
+  const mongodbUrl = 'mongodb+srv://phucnguyendx9012:Phuc2001@cluster0.ex67or6.mongodb.net/'
+  const client = new MongoClient(mongodbUrl)
+  try {
+    await client.connect()
+    console.log('connect to MongoDB successfully');
 
-app.listen(3000)
+    const teacherCollection = client.db('appChamCong').collection('teachers')
+    const teacher = await teacherCollection.find().toArray()
+    console.log("🚀 ~ file: index.js:19 ~ main ~ teacher:", teacher)
+  } catch (error) {
+    console.log('Error connect to MongoDB ', error);
+
+  }
+
+
+
+  app.use(express.json())
+  app.use('/api/v1/auth', authRouter)
+  app.use("/students", studentRouter)
+  app.use("/teachers", teacherRouter)
+  app.use("/api/v1/todos", todoRouter)
+  app.get('/', function (req, res) {
+    res.send('Hello World')
+  })
+
+
+
+  app.listen(3000)
+}
+
+main()
--- routers/teacher.js
+++ routers/teacher.js
@@ -1,7 +1,21 @@
 import express from 'express'
+import { MongoClient } from 'mongodb'
+
 const router = express.Router();
-router.get('/', function (req, res) {
-    res.send('hasha')
+router.get('/', async function (req, res) {
+    const mongodbUrl = 'mongodb+srv://phucnguyendx9012:Phuc2001@cluster0.ex67or6.mongodb.net/'
+    const client = new MongoClient(mongodbUrl)
+
+    try {
+        await client.connect()
+        console.log('connect to MongoDB successfully');
+
+        const teacherCollection = client.db('appChamCong').collection('teachers')
+        const teacher = await teacherCollection.find().toArray()
+        res.json(teacher)
+    } catch (error) {
+
+    }
 
 })
 router.get('/edit', function (req, res) {
