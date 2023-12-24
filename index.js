1f6668adb2ff844aa7849f55936338faf6951001
phucbndx <phucnguyendx9012@gmail.com>
2023-12-24 11:10:51 +0700

    fix path

--- index.js
+++ index.js
@@ -1,42 +1,33 @@
-import express from "express";
-import authRouter from './routers/auth.js'
-import studentRouter from './routers/student.js'
-import teacherRouter from './routers/teacher.js'
-import todoRouter from './routers/todoRouter.js'
+import express from 'express';
+import authRouter from '../learnNodejs/src/routers/auth.js';
+import studentRouter from '../learnNodejs/src/routers/student.js';
+import teacherRouter from '../learnNodejs/src/routers/teacher.js';
+// import todoRouter from '../learnNodejs/src/routers/todoRouter.js';
 
-import { config } from 'dotenv'
-import { client } from "./config/connectDB.js";
-config()
-const app = express()
+import { config } from 'dotenv';
+import { client } from '../learnNodejs/src/config/connectDB.js';
+config();
+const app = express();
 async function main() {
   try {
-
-
-    await client.connect()
+    await client.connect();
     console.log('connect to MongoDB successfully');
 
     // const teacherCollection = client.db('appChamCong').collection('teachers')
 
-    app.use(express.json())
-    app.use('/api/v1/auth', authRouter)
-    app.use("/students", studentRouter)
-    app.use("/teachers", teacherRouter)
-    app.use("/api/v1/todos", todoRouter)
+    app.use(express.json());
+    app.use('/api/v1/auth', authRouter);
+    app.use('/students', studentRouter);
+    app.use('/teachers', teacherRouter);
+    // app.use('/api/v1/todos', todoRouter);
     app.get('/', function (req, res) {
-      res.send('Hello World')
-    })
+      res.send('Hello World');
+    });
 
-
-
-    app.listen(process.env.PORT || 3000)
+    app.listen(process.env.PORT || 3000);
   } catch (error) {
     console.log('Error connect to MongoDB ', error);
-
   }
-
-
-
-
 }
 
-main()
+main();
--- src/routers/todoRouter.js
+++ src/routers/todoRouter.js
@@ -1,105 +1,100 @@
-import express from 'express'
-import fs from 'fs'
-import path from 'path'
+import express from 'express';
+import fs from 'fs';
+import path from 'path';
 import { v4 as uuidv4 } from 'uuid';
 import loggerMiddleware from '../middlewares/loggerMiddleware.js';
-import jwt from 'jsonwebtoken'
+import jwt from 'jsonwebtoken';
 import authMiddleware from '../middlewares/authMiddleware.js';
 
 const todoRouter = express.Router();
-const todoPath = path.join("resources", "todoResource.json")
-const fileData = fs.readFileSync(todoPath, 'utf8')
-const dataJson = JSON.parse(fileData)
-const JWT_SECRET = "MY_SECRET_KEY"
+const todoPath = path.join('resources', 'todoResource.json');
+console.log('🚀 ~ file: todoRouter.js:10 ~ todoPath:', todoPath);
+const fileData = fs.readFileSync(todoPath, 'utf8');
+const dataJson = JSON.parse(fileData);
+const JWT_SECRET = 'MY_SECRET_KEY';
 
 todoRouter.get('/', authMiddleware, (req, res) => {
-
-    try {
-        res.status(200).json({
-            success: true,
-            data: dataJson
-        })
-    } catch (error) {
-        res.status(500).json({
-            message: error
-        })
-    }
-
-})
-
+  try {
+    res.status(200).json({
+      success: true,
+      data: dataJson
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error
+    });
+  }
+});
 
 todoRouter.get('/:todoId', authMiddleware, (req, res) => {
-    try {
-        const id = req.params.todoId
-        let result = dataJson.filter((el) => el.id === (id))
-        res.status(200).json({
-            success: true,
-            data: result
-        })
-    } catch (error) {
-        res.status(500).json({
-            message: error
-        })
-    }
-})
+  try {
+    const id = req.params.todoId;
+    let result = dataJson.filter(el => el.id === id);
+    res.status(200).json({
+      success: true,
+      data: result
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error
+    });
+  }
+});
 
 todoRouter.post('/', authMiddleware, (req, res) => {
-    try {
-        const data = req.body
-        const newData = {
-            id: uuidv4(),
-            title: data.title,
-            isCompleted: data.isCompleted
-        }
+  try {
+    const data = req.body;
+    const newData = {
+      id: uuidv4(),
+      title: data.title,
+      isCompleted: data.isCompleted
+    };
 
-        let result = [...dataJson, newData]
-        fs.writeFileSync(todoPath, JSON.stringify(result));
-        console.log("🚀 ~ file: todoRouter.js:42 ~ todoRouter.post ~ result:", result)
-        res.status(200).json({
-            success: true,
-            data: result
-        })
-
-    } catch (error) {
-        res.status(500).json({
-            message: error
-        })
-    }
-
-})
+    let result = [...dataJson, newData];
+    fs.writeFileSync(todoPath, JSON.stringify(result));
+    console.log(
+      '🚀 ~ file: todoRouter.js:42 ~ todoRouter.post ~ result:',
+      result
+    );
+    res.status(200).json({
+      success: true,
+      data: result
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error
+    });
+  }
+});
 
 todoRouter.put('/:todoId', authMiddleware, (req, res) => {
-    try {
-
-        const data = req.body
-        const id = req.params.todoId
-        let result = dataJson.map((el) => {
-            if (el.id === id) {
-                return { ...data, id: id }
-            } else {
-                return el
-            }
-        })
-        fs.writeFileSync(todoPath, JSON.stringify(result))
-        res.status(200).send('oke')
-    } catch (error) {
-
-    }
-
-})
+  try {
+    const data = req.body;
+    const id = req.params.todoId;
+    let result = dataJson.map(el => {
+      if (el.id === id) {
+        return { ...data, id: id };
+      } else {
+        return el;
+      }
+    });
+    fs.writeFileSync(todoPath, JSON.stringify(result));
+    res.status(200).send('oke');
+  } catch (error) {}
+});
 
 todoRouter.delete('/:todoId', authMiddleware, (req, res) => {
-    try {
-        const id = req.params.todoId
-        console.log("🚀 ~ file: todoRouter.js:69 ~ todoRouter.delete ~ id:", id)
-        const result = dataJson.filter((el) => el.id !== id)
-        fs.writeFileSync(todoPath, JSON.stringify(result))
-        res.status(200).send('ok')
-    } catch (error) {
-        res.status(500).json({
-            message: error
-        })
-    }
-})
+  try {
+    const id = req.params.todoId;
+    console.log('🚀 ~ file: todoRouter.js:69 ~ todoRouter.delete ~ id:', id);
+    const result = dataJson.filter(el => el.id !== id);
+    fs.writeFileSync(todoPath, JSON.stringify(result));
+    res.status(200).send('ok');
+  } catch (error) {
+    res.status(500).json({
+      message: error
+    });
+  }
+});
 
-export default todoRouter
+export default todoRouter;
