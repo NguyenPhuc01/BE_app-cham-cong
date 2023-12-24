c4038374991f6af02393991fb77fa35cd4794c60
phucbndx <phucnguyendx9012@gmail.com>
2023-12-24 22:36:52 +0700

    add log

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const express = require('express');const authRouter = require('./src/routers/auth.js');
+const express = require('express');
+const authRouter = require('./src/routers/auth.js');
 const studentRouter = require('./src/routers/student.js');
 const teacherRouter = require('./src/routers/teacher.js');
 
@@ -8,18 +9,26 @@ dotenv.config();
 const app = express();
 async function main() {
   try {
+    console.log('111');
+
     await client.connect();
     console.log('file: index.js:7 ~ connect to MongoDB successfully');
 
     // const teacherCollection = client.db('appChamCong').collection('teachers')
-
+    console.log('22222');
     app.use(express.json());
+    console.log('333');
+
     app.use('/api/v1/auth', authRouter);
+    console.log('24444');
+
     app.use('/students', studentRouter);
+    console.log(555555);
     app.use('/teachers', teacherRouter);
     app.get('/', function (req, res) {
       res.send('Hello World');
     });
+    console.log(6666666);
 
     app.listen(process.env.PORT || 3000);
   } catch (error) {
