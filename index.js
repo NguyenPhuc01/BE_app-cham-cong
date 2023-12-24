6c59e0d37097f849f804be0425fd2d98a6a96f0e
phucbndx <phucnguyendx9012@gmail.com>
2023-12-24 12:07:00 +0700

    update

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import authRouter from '../learnNodejs/src/routers/auth.js';
-import studentRouter from '../learnNodejs/src/routers/student.js';
-import teacherRouter from '../learnNodejs/src/routers/teacher.js';
-// import todoRouter from '../learnNodejs/src/routers/todoRouter.js';
+import authRouter from './src/routers';
+import studentRouter from './src/routers/student.js';
+import teacherRouter from './src/routers/teacher.js';
+import todoRouter from './src/routers/todoRouter.js';
 
 import { config } from 'dotenv';
-import { client } from '../learnNodejs/src/config/connectDB.js';
+import { client } from './src/config/connectDB.js';
 config();
 const app = express();
 async function main() {
@@ -19,7 +19,7 @@ async function main() {
     app.use('/api/v1/auth', authRouter);
     app.use('/students', studentRouter);
     app.use('/teachers', teacherRouter);
-    // app.use('/api/v1/todos', todoRouter);
+    app.use('/api/v1/todos', todoRouter);
     app.get('/', function (req, res) {
       res.send('Hello World');
     });
